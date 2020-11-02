12d065feecba90dda46f6fa23081d3a48c14ad8b
bamaro <amaro.belen.a@gmail.com>
2020-11-02 16:32:31 -0300

    Add payment method

---
+++ Doppler.Sap/Enums/PayTermsGroup.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doppler.Sap.Enums
+{
+    public enum PayTermsGroup
+    {
+        DEFAULT = 5,
+        MP = 6
+    }
+}
